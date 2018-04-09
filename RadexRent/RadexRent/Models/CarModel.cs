f22930207216a44c73f87a4d6c2740dfbb03c0c2
DESKTOP-4HJ1KAG\Admin <email@dot.com>
2018-04-09 18:42:15 +0200

    Added CarModel

---
+++ RadexRent/RadexRent/Models/CarModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RadexRent.Models
+{
+    public class CarModel
+    {
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public string Company { get; set; }
+        public string ProductionYear { get; set; }
+        public string Color { get; set; }
+
+
+    }
+}
