ce2fc3f55b2a73b71a0bdee2d01998ecf7e152fa
Radek D <radek.db@gmail.com>
2018-04-11 18:02:05 +0200

    oldcommit

--- RadexRent/RadexRent/Models/CarReservation.cs
+++ RadexRent/RadexRent/Models/CarReservation.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace RadexRent.Models
 {
     public class CarReservation
     {
         public int Id { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public string StartPlace { get; set; }
